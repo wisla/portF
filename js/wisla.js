758cc8f05e2ca850a63421cee40b6d12f5b2f80e
wisla <andrzej.wislowski@combeenut.com>
2015-08-25 09:12:02 +0200

    first

--- js/wisla.js
+++ js/wisla.js
@@ -1,3 +1,61 @@
-/**
- * Created by ARGUS on 2015-08-19.
- */
+jQuery(document).ready(function($) {
+
+    // active menu
+    var lastId,
+        topMenu = $("#top-menu"),
+        topMenuHeight = topMenu.outerHeight(),
+        menuItems = topMenu.find("a"),
+        scrollItems = menuItems.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
+    menuItems.click(function(e){
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+52;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        e.preventDefault();
+    });
+
+    $(window).scroll(function(){
+        var fromTop = $(this).scrollTop()+topMenuHeight;
+
+        var cur = scrollItems.map(function(){
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href=#"+id+"]").parent().addClass("active");
+        }
+    });
+
+    // hide #back-top first
+    jQuery("#back-top").hide();
+
+    // fade in #back-top
+    jQuery(function () {
+        jQuery(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 100) {
+                jQuery('#back-top').fadeIn();
+            } else {
+                jQuery('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        jQuery('#back-top a').click(function () {
+            jQuery('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+} );
